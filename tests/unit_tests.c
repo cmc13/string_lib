77e2e6f81a55752991879510c9002fb04ccb6e24
Cole <spectre13@gmail.com>
2013-12-01 22:36:12 -0600

    improve flow of unit_tests bootstrapper.

--- tests/unit_tests.c
+++ tests/unit_tests.c
@@ -12,17 +12,15 @@ int main()
 		CU_SUITE_INFO_NULL
 	};
 
-	if (CU_initialize_registry() != CUE_SUCCESS)
-		return CU_get_error();
-
-	if (CU_register_suites(suites) != CUE_SUCCESS)
+	if (CU_initialize_registry() == CUE_SUCCESS)
 	{
-		CU_cleanup_registry();
-		return CU_get_error();
+		if (CU_register_suites(suites) == CUE_SUCCESS)
+		{
+			CU_basic_set_mode(CU_BRM_VERBOSE);
+			CU_basic_run_tests();
+		}
 	}
 
-	CU_basic_set_mode(CU_BRM_VERBOSE);
-	CU_basic_run_tests();
 	CU_cleanup_registry();
 
 	return CU_get_error();
